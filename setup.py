bb79d6b187292f924b9fa6d7232c3acbd6825873
Ala Bouali <trap.leader.123@gmail.com>
2021-11-25 21:21:17 -0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sanitizy",
-    version="1.1.0",
+    version="1.1.1",
     author="AlaBouali",
     author_email="trap.leader.123@gmail.com",
     description="Helpful module to secure flask apps",
